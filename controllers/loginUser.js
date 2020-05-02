c2b2fa558dbf74c487ff5df780c3ad5b5ebac668
twinstwins <weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jp>
2020-05-02 11:54:52 +0900

    ログイン用コントローラー定義

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,4 +1,36 @@
-module.exports = ('auth/login',function(req,res){
-  console.log("ログインします！")
-    res.render('../views/login');
-});
+const Sequelize = require('sequelize');
+const models = require('../models')
+const User = require('../models/user.js')
+const bcrypt = require('bcrypt')
+
+module.exports = (req,res) => {
+    var matched_users = models.User.findAll({
+        where: Sequelize.and(
+            {email: req.body.email},
+        )
+    });
+    matched_users.then(function(users){
+        if(users.length > 0){
+            let user = users[0];
+            let passwordHashed = user.password;
+            if(bcrypt.compareSync(req.body.password,passwordHashed)){
+                req.session.userId = user.id
+                req.session.email = req.body.email;
+                console.log("ログインに成功")
+                console.log(req.session.url)
+                if (req.session.url == undefined) {
+                  res.render("../views/post");
+                }
+                res.redirect(req.session.url);
+            }
+            else{
+                console.log("ユーザー登録にリダイレクト")
+                res.redirect('/auth/register');
+            }
+        }
+        else{
+        　  console.log("ログインにリダイレクト")
+            res.redirect('/auth/login');
+        }
+    });
+};
