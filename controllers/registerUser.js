77c1da023d775e2347936dabea7270434e31ee2c
twinstwins <weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jp>
2020-05-02 11:52:07 +0900

    登録ページ用コントローラー作成

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -2,3 +2,7 @@ module.exports = ('auth/register',function(req,res){
   console.log('登録します！')
     res.render('../views/register',{errors: ""});
 });
+
+// 'auth/register'の形でgetで指定されるURLを記述しているが、
+// ルーティングとの対応を明確にするための明記である。
+// 動作上、必ずしも必要はない。
