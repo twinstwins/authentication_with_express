cc8ca6be609e44b47f9fed477867a7b6c1bb4d5f
twinstwins <weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jp>
2020-05-02 11:54:21 +0900

    新規ユーザー登録用コントローラー作成

---
+++ controllers/createUser.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const models = require('../models')
+const User = require('../models/user.js')
+const bcrypt = require('bcrypt')
+
+// User.findAll　が動かねえええ！
+
+module.exports = (req,res) => {
+  console.log("！！！！！！！！！！！！！！POSTメソッド送信！！！！！！！！！！！！！！！！！！！！")
+  var matched_users = models.User.findAll({
+     where:  Sequelize.or(
+             {name: req.body.name},
+             {email: req.body.email}
+         )
+ });
+ matched_users.then(function(users){
+     if(users.length == 0){
+         const passwordHashed = bcrypt.hashSync(req.body.password,3);
+         models.User.create({
+             name: req.body.name,
+             email: req.body.email,
+             password: passwordHashed
+         }).then(function(){
+             let newSession = req.session;
+             newSession.email = req.body.email;
+             res.redirect('/');
+         });
+     }
+     else{
+         res.render('../views/register',{errors: "name or Email already in user"});
+     }
+ })
+};
